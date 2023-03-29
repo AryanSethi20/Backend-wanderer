cb0f6a9102fcf8d8a2462321d8afa5cbf7e7d978
AryanSethi20 <aryansethi20@gmail.com>
2023-03-29 15:44:41 +0800

    Removed utils for uuid for each model

--- utils/model_abstracts.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from django.db import models
-
-
-class Model(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-
-    class Meta:
-        abstract = True
