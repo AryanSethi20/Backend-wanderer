fa8a3c9a370aaa0144d96ec75389e1dbe5cfc409
AryanSethi20 <aryansethi20@gmail.com>
2023-04-05 14:00:14 +0800

    commit to run server on different machines

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
+from .views import ride
 
 urlpatterns = [
-    path('<int:index>', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('rides/', ride.as_view()),
 ]
